cba972e629e2a772ff2902977d8db063c800c335
Sidorcenko <m20405657@gmail.com>
2024-05-25 18:59:00 +0300

    Apģērbs un Toggle

--- Assets/Scripts/ImageScript.cs
+++ Assets/Scripts/ImageScript.cs
@@ -5,8 +5,15 @@ using UnityEngine.UI;
 
 public class ImageScript : MonoBehaviour
 {
-    public GameObject chupcha1;
-    public GameObject chupcha2;
+    public GameObject Krekli1;
+    public GameObject Krekli2;
+    public GameObject Krekli3;
+    public GameObject Bikses1;
+    public GameObject Bikses2;
+    public GameObject Bikses3;
+    public GameObject Apavi1;
+    public GameObject Apavi2;
+    public GameObject Apavi3;
     public GameObject mainigaisAttels;
     public Sprite[] atteluMasivs;
 
@@ -16,5 +23,25 @@ public class ImageScript : MonoBehaviour
         } if (skaitlis == 1){
             mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
         }
+    }
+    public void showKrekli(bool value)
+    {
+        Krekli1.SetActive(value);
+        Krekli2.SetActive(value);
+        Krekli3.SetActive(value);
+    }
+
+    public void showBikses(bool value)
+    {
+        Bikses1.SetActive(value);
+        Bikses2.SetActive(value);
+        Bikses3.SetActive(value);
+    }
+
+    public void showApavi(bool value)
+    {
+        Apavi1.SetActive(value);
+        Apavi2.SetActive(value);
+        Apavi3.SetActive(value);
     }
 }
